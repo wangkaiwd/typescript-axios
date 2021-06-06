76e5aed59f0c8105e2b626242a6380c27efc284e
wangkaiwd <1162486176@qq.com>
2021-06-06 18:22:37 +0800

    feat: run demo successfully

--- website/server.js
+++ website/server.js
@@ -9,7 +9,7 @@ const compiler = webpack(config);
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
-app.use(webpackDevMiddleware(compiler, { writeToDisk: true }));
+app.use(webpackDevMiddleware(compiler));
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
