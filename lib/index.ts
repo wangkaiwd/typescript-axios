56a7c523be6d50326a14a8650df4cafeac175ab9
wangkaiwd <1162486176@qq.com>
2021-12-07 21:22:43 +0800

    feat: why need to export types in index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -3,6 +3,7 @@ import axios from "./axios";
 // 1. axios can invoke as a function directly
 // 2. axios is an object support http verb method
 
+// export all typescript types for consumer, otherwise consumer can't find correspond type location
 export * from "./types";
 
 export default axios;
